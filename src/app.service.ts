3f9a5edfd77cf3f85d624b97a1df585803b0e812
robinyeon <robin.yeon@gmail.com>
2024-04-02 14:17:34 +0900

    test: fix to finish github actions PR test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    var a = 1;
     return 'Hello World from Robin Yeon for PR github action test!';
   }
 }
