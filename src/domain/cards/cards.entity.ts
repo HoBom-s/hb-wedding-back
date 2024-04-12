1fe46a4029f786202d8775a745d7055a4b33ea16
robinyeon <robin.yeon@gmail.com>
2024-04-12 22:46:04 +0900

    feat: add card 'title' column

--- src/domain/cards/cards.entity.ts
+++ src/domain/cards/cards.entity.ts
@@ -1,12 +1,29 @@
-import { Column, DeleteDateColumn, Entity, ManyToOne } from "typeorm";
+import {
+    Column,
+    DeleteDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+} from "typeorm";
 import { Common } from "src/entities/common.entity";
 import { CardDirection } from "src/types/entities/card.type";
 import { User } from "../users/entity/users.entity";
 
 @Entity("card")
 export class Card extends Common {
-    @ManyToOne(() => User)
+    @ManyToOne(() => User, { eager: false })
+    @JoinColumn({ name: "userId" })
     user: User;
+
+    @Column()
+    userId: string;
+
+    @Column({
+        type: "varchar",
+        length: "40",
+        nullable: false,
+    })
+    title: string;
 
     @Column({
         type: "enum",
@@ -16,5 +33,5 @@ export class Card extends Common {
     direction: CardDirection;
 
     @DeleteDateColumn()
-    deleted_at: Date;
+    deletedAt: Date;
 }
