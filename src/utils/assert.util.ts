a5660281ae2fe63d2dda55074b2fdfd4813985e5
foxmon <jjockrod@naver.com>
2024-04-12 15:52:24 +0900

    assert util

--- src/utils/assert.util.ts
+++ src/utils/assert.util.ts
@@ -1,6 +1,17 @@
+/**
+ * `assert.util.ts`
+ *
+ * Assert를 수행하는 Util함수를 정의하도록 한다.
+ */
 export class AssertUtil {
     readonly _instance = Symbol.for("AssertUtil");
 
+    /**
+     * 어떠한 값이 들어올지 모르지만, `string` type이 아니라면
+     * TypeError를 발생시키도록 한다.
+     *
+     * @param {unknown} v
+     */
     public static assertString(v: unknown): asserts v is string {
         if (typeof v !== "string" || !v) {
             throw TypeError(`Assert fail with ${typeof v} !`);
