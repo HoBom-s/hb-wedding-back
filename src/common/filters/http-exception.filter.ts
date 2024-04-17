21fb0f408013c16987919ba605d174b2e0790835
robinyeon <robin.yeon@gmail.com>
2024-04-18 00:02:47 +0900

    chore: adding generic

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -4,12 +4,13 @@ import {
     ExceptionFilter,
     HttpException,
 } from "@nestjs/common";
+import { Response } from "express";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
-        const response = ctx.getResponse();
+        const response = ctx.getResponse<Response>();
         const status = exception.getStatus();
 
         return response.status(status).json({
