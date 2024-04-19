85af04ecd8dfb7a46bf8c7a0073cedfbd89f6539
robinyeon <robin.yeon@gmail.com>
2024-04-20 00:13:24 +0900

    fix: type ErrorCode

--- src/common/exceptions/base.exception.ts
+++ src/common/exceptions/base.exception.ts
@@ -1,9 +1,12 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
+import { CardErrorEnum, UserErrorEnum } from "../enums";
+
+type ErrorCode = CardErrorEnum | UserErrorEnum;
 
 export class BaseException extends HttpException {
     msg: string;
 
-    constructor(msg: string, errorCode: string, statusCode: HttpStatus) {
+    constructor(msg: string, errorCode: ErrorCode, statusCode: HttpStatus) {
         super(errorCode, statusCode);
         this.msg = msg;
     }
